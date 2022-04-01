47b8ab84d5710d6b9d9005d33c74d5747d746611
Ozan Özten <24881868+oznoztn@users.noreply.github.com>
2022-04-02 00:34:13 +0300

    CodeWars

---
+++ LeetCode/CodeWars/TwoToOne.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using Xunit;
+
+namespace LeetCode.CodeWars
+{
+    /// <summary>
+    /// Take 2 strings s1 and s2 including only letters from ato z.
+    /// Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+    ///
+    /// a = "xyaabbbccccdefww"
+    /// b = "xxxxyyyyabklmopq"
+    /// longest(a, b) -&gt; "abcdefklmopqwxy"
+    /// 
+    /// a = "abcdefghijklmnopqrstuvwxyz"
+    /// longest(a, a) -&gt; "abcdefghijklmnopqrstuvwxyz"
+    /// 
+    /// </summary>
+    public class TwoToOne
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal("aehrsty", Execute("aretheyhere", "yestheyarehere"));
+            Assert.Equal("abcdefghilnoprstu", Execute("loopingisfunbutdangerous", "lessdangerousthancoding"));
+            Assert.Equal("acefghilmnoprstuy", Execute("inmanylanguages", "theresapairoffunctions"));
+        }
+
+        private string Execute(string str1, string str2)
+        {
+            var chars = new char[128];
+
+            for (int i = 0; i < str1.Length; i++)
+                chars[str1[i]] = str1[i];
+
+            for (int i = 0; i < str2.Length; i++)
+                chars[str2[i]] = str2[i];
+
+            var stringBuilder = new StringBuilder();
+            
+            // a b c d e _ f g h j k l m n
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (!char.IsLetter(chars[i]))
+                    continue;
+                else
+                    stringBuilder.Append(chars[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ LeetCode/CodeWars/TwoToOne_CustomizedComplex.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using Xunit;
+
+namespace LeetCode.CodeWars
+{
+    /// <summary>
+    /// Ben işi biraz daha zorlaştırdım.
+    /// Bu diğerine nazaran sadece "alfabetik yönden geçerli mümkün olan en uzun stringi bulur.
+    ///
+    /// Alfabetik yönden geçerli olması demek en uzun stringi oluşturan harflerin arasında boşluk olmaması demektir.
+    /// 
+    /// </summary>
+    public class TwoToOne_CustomizedComplex
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal("abcdefg", Execute("abc", "aabbccddeeffgggg"));
+            Assert.Equal("pqrstuvwxyz", Execute("abc", "aabbccddeeffggggmnpqrstuvwxyz"));
+        }
+
+        private string Execute(string str1, string str2)
+        {
+            var chars = new char[128];
+
+            for (int i = 0; i < str1.Length; i++)
+                chars[str1[i]] = str1[i];
+
+            for (int i = 0; i < str2.Length; i++)
+                chars[str2[i]] = str2[i];
+
+            int longestStart = 0;
+            int longestLength = 0;
+
+            int start = 0;
+            int length = 0;
+
+            // a b c d e _ f g h j k l m n
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] < chars[i + 1] && !char.IsWhiteSpace(chars[i + 1]))
+                {
+                    if (start == 0)
+                        start = i + 1;
+
+                    length += 1;
+                }
+                else
+                {
+                    if (length > longestLength)
+                    {
+                        longestStart = start;
+                        longestLength = length;
+                    }
+
+                    start = 0;
+                    length = 0;
+
+                }
+            }
+
+
+            var counter = 0;
+            var stringBuilder = new StringBuilder();
+            while (counter != longestLength)
+            {
+                var ch = chars[longestStart + counter];
+                stringBuilder.Append(ch);
+
+                counter++;
+            }
+
+            var result = stringBuilder.ToString();
+            return result;
+        }
+    }
+}
