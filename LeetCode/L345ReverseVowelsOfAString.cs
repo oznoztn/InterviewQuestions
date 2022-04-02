165ce3799399a7a537b0c67bfc6e009fa8b7a1c5
Ozan Özten <24881868+oznoztn@users.noreply.github.com>
2022-04-03 01:25:14 +0300

    Create L345ReverseVowelsOfAString.cs

---
+++ LeetCode/L345ReverseVowelsOfAString.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 20. Valid Parentheses
+    /// </summary>
+    public class L345ReverseVowelsOfAString
+    {
+        [Fact]
+        public void Test()
+        {
+            IsValid("aA").Should().Be("Aa");
+            IsValid("hello").Should().Be("holle");
+            IsValid("leetcode").Should().Be("leotcede");
+        }
+
+        public string IsValid(string s)
+        {
+            StringBuilder strb = new StringBuilder();
+
+            // IT SAYS "PRINTABLE ASCII CHARACTERS ONLY"
+            foreach (char ch in s)
+                if(ch >= 32 && ch <= 127)
+                    strb.Append(ch);
+
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start < end)
+            {
+                if (strb[start] == 'a' || 
+                    strb[start] == 'e' ||
+                    strb[start] == 'i' || 
+                    strb[start] == 'o' ||
+                    strb[start] == 'u' || 
+                    strb[start] == 'A' ||
+                    strb[start] == 'E' || 
+                    strb[start] == 'I' || 
+                    strb[start] == 'O' || 
+                    strb[start] == 'U')
+                {
+                    while (true)
+                    {
+                        if (strb[end] == 'a' || 
+                            strb[end] == 'e' || 
+                            strb[end] == 'i' || 
+                            strb[end] == 'o' || 
+                            strb[end] == 'u' || 
+                            strb[end] == 'A' || 
+                            strb[end] == 'E' || 
+                            strb[end] == 'I' || 
+                            strb[end] == 'O' || 
+                            strb[end] == 'U')
+                        {
+                            (strb[start], strb[end]) =  (strb[end], strb[start]);
+
+                            end--;
+                            break;
+                        }
+                        else
+                        {
+                            end--;
+                        }
+                    }
+                }
+
+                start++;
+            }
+
+            return strb.ToString();
+        }
+    }
+}
